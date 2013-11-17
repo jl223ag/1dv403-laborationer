38090d3b742c2f25aa0b8930ebb44ee48b5a9062
OpenShift guest <jl223ag@student.lnu.se>
2013-11-17 03:56:45 -0500

    födelsedagen

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -4,32 +4,45 @@ window.onload = function(){
 
 	
 	var birthday = function(date){
-	
-        var userDate = new Date(date);
-        var now = new Date();
-        
-        userDate.setFullYear(now.getFullYear());
-        
-        var days = Math.ceil((userDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
-        if (days < 0){
-            var nextYear = new Date();
-            nextYear.setFullYear(now.getFullYear() + 1);
-            days = (days * -1) + (nextYear.getTime() - now.getTime()) / (1000 * 60 * 60 * 24);
-        }
+	 
+	errorcheck(date);
+    
+    var userDate = new Date(date);
+    var now = new Date();
+    
+    userDate.setFullYear(now.getFullYear());
+    
+    var days = Math.ceil((userDate.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
+    if (days < 0){
+        var nextYear = new Date();
+        nextYear.setFullYear(now.getFullYear() + 1);
+        days = days + (nextYear.getTime() - now.getTime()) / (1000 * 60 * 60 * 24);
+    }
 
     return days;
 	
+        function errorcheck(thedate){
+        
+        var check = +thedate.substr(0,4);
+        var check2 = +thedate.substr(5,2);
+        var check3 = +thedate.substr(8,2);
+        
+        if (thedate.charAt(4) != "-" || thedate.charAt(7) != "-" || isNaN(check) || isNaN(check2) || isNaN(check3)){
+            
+            throw new Error("Detta är inget giltigt datum mannen!");
+        }
+        if (check2 < 1 ||check2 > 12){
+            
+            throw new Error("Månaden finns inte");
+        }
+        if (check3 < 1 || check3 > 31){
+            
+            throw new Error("Dagen finns inte");
+        }
+        
+    };
 	
-	
-	
-
-
-	
-
-
-
-
-	};
+};
 	// ------------------------------------------------------------------------------
 
 
@@ -45,7 +58,7 @@ window.onload = function(){
 		p.classList.remove( "error");
 
 		try {
-			var answer = birthday(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+			var answer = birthday(input.value); // Läser in texten från textrutan och skickar till funktionen "convertString"
 			var message;
 			switch (answer){
 				case 0: message = "Grattis på födelsedagen!";
