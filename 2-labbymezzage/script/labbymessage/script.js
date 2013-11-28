d9ee179a5b0c34406e6264d977f0d98866337412
jl223ag <jl223ag@student.lnu.se>
2013-11-28 19:43:48 +0100

    remove message tillagd

--- 2-labbymezzage/script/labbymessage/script.js
+++ 2-labbymezzage/script/labbymessage/script.js
@@ -1,5 +1,6 @@
 "use strict"
 
+
 function Message(text, date) {
 
     this.getText = function () { return text; };
@@ -23,43 +24,70 @@ Message.prototype.getDateText = function (now) {
 var labby = {
     count: 0,
     messages: [],
-    renderMessage: function(messageId){
+    countDisplayer: function (msgcount) {
+
+        msgcount = document.querySelector("#numberMessages");
+        msgcount.innerHTML = "Antal meddelanden: " + labby.count;
+    },    
+    removeMessage: function(messageId){
+
+        labby.messages.splice(messageId, 1);
+        labby.count--;
+        labby.countDisplayer();
+        labby.renderMessages();        
+    },
+    renderMessage: function (messageId) { // skapar ett meddelande
 
         var wall = document.querySelector("#textWall");
         var div = document.createElement("div");
         var p = document.createElement("p");
+        var a = document.createElement("a");
+        var ab = document.createElement("a");
+        var clock = document.createElement("img");        
+        var kill = document.createElement("img");
+
+        a.setAttribute("href", "#");
+        a.onclick = function () {
+            labby.removeMessage(messageId);
+            return false;
+        }
+        ab.setAttribute("href", "#");
+
+        clock.src = "pictures/clock.png";
+        kill.src = "pictures/x.png";
+
         wall.appendChild(div);
         div.appendChild(p);
+        div.appendChild(ab);
+        div.appendChild(a);
+        
         p.innerHTML = labby.messages[messageId];
-
-
+        ab.appendChild(clock);
+        a.appendChild(kill);
         console.log(labby.messages[messageId]);
     },
     renderMessages: function () {
 
-        document.querySelector("#textWall").innerHTML = "";
+        document.querySelector("#textWall").innerHTML = ""; // tar bort alla meddelanden
 
-        for (var i = 0; i < labby.messages.length; i++) {
+        for (var i = 0; i < labby.messages.length; i++) { // skriver ut dem på nytt
             labby.renderMessage(i);
         }
-
     },
     run: function () {
+                      
+        var mess = new Message();
+        var sendmsg = document.querySelector("#abutton");
 
-	}
+        sendmsg.onclick = function () {
+
+            var input = document.querySelector("#theText").value;
+            labby.messages.push(input); // in med inputen i arrayen
+            labby.count++;
+            labby.renderMessages();
+            labby.countDisplayer();
+        };
+    }
 };
 
 window.onload = labby.run;
-
-function getInput() {
-    var mess = new Message();
-    mess.getDate = new Date();
-    
-    var input = document.querySelector("#theText").value;   
-    labby.messages.push(input); // in med inputen i arrayen
-
-    labby.renderMessages();
-    
-    labby.count += 1;
-
-};
